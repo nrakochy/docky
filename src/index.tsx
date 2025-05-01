a9ef09838d4a5fdf75619ef9e163145c0de6d511
Nick Rakochy <nrakochy@anduril.com>
2025-05-01 15:01:57 -0500

    Add theme selector

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,16 @@ import { StrictMode, useCallback, useEffect, useLayoutEffect, useState } from 'r
 import * as ReactDOMClient from 'react-dom/client';
 import './styles.css';
 import 'dockview/dist/styles/dockview.css';
-import { themeDark } from "dockview";
+import {
+    themeDark,
+    themeLight,
+    themeVisualStudio,
+    themeAbyss,
+    themeDracula,
+    themeReplit,
+    themeAbyssSpaced,
+    themeLightSpaced
+} from "dockview";
 
 import App from './app';
 import { Themer } from './Themer';
@@ -10,6 +19,18 @@ import { CSSThemeObj } from './types';
 import { DockviewTheme } from 'dockview';
 
 const CUSTOM_CLASE = "importantcustom";
+const THEMES = new Map<string, DockviewTheme>([
+    themeDark,
+    themeLight,
+    themeVisualStudio,
+    themeAbyss,
+    themeDracula,
+    themeReplit,
+    themeAbyssSpaced,
+    themeLightSpaced
+].map(theme => [theme.name, theme]));
+
+type ThemeCodeProps = { themeStr: string, activeTheme: ReturnType<typeof useDockviewTheme>[0]['name']; onThemeSelect: ReturnType<typeof useDockviewTheme>[1] };
 
 // lookup the relevant element and injected a dynamic CSS classname
 const useHackCSSClass = (dockThemeName: string) => {
@@ -54,25 +75,60 @@ const useOnClick = (themeStr: string) => {
     return [clicked, onClick] as const;
 }
 
-function Themeable() {
-    const [dock] = useState<DockviewTheme>(themeDark);
-    const [theme, setTheme, themeStr] = useHackCSSClass(dock.name);
+const useDockviewTheme = () => {
+    const defaults = themeDark;
+    const [theme, setTheme] = useState<DockviewTheme>(defaults);
+
+    const onClick = useCallback((name: string) => {
+        setTheme(THEMES.get(name) ?? defaults);
+    }, []);
+
+    return [theme, onClick] as const;
+}
+
+function ThemeSelector(props: Omit<ThemeCodeProps, "themeStr">) {
+    const { onThemeSelect, activeTheme } = props;
+    return (
+        <select value={activeTheme} className="custom-select" onChange={(e) => onThemeSelect(e.target.value)}>
+            {[...THEMES].map(([t]) => (
+                <option value={t}>
+                    <span>{t}</span>
+                </option>
+            ))}
+        </select>
+    )
+}
+
+
+function ThemeCode(props: ThemeCodeProps) {
+    const { themeStr, ...rest } = props;
     const [clicked, onClick] = useOnClick(themeStr);
+    return (<div style={{ padding: "8px", display: "flex", flexDirection: "column", gap: "8px", overflow: "auto" }}>
+        <section style={{ padding: "8px", display: "flex", flex: "1", gap: "8px", justifyContent: "space-between", alignItems: "center" }} >
+            <h2 id="code">Code</h2>
+            <div>
+                <ThemeSelector {...rest} />
+                {themeStr ? <button style={{ cursor: "pointer" }} disabled={clicked} onClick={onClick}>Copy</button> : null}
+            </div>
+        </section>
+        <code>
+            <pre>
+                {themeStr}
+            </pre>
+        </code>
+    </div>)
+
+}
+
+
+function Themeable() {
+    const [dock, setDockview] = useDockviewTheme();
+    const [theme, setTheme, themeStr] = useHackCSSClass(dock.name);
 
     return (<div className="app themer">
         <div className="theme-container">
             <Themer theme={theme} setTheme={setTheme} />
-            <div style={{ padding: "8px", display: "flex", flexDirection: "column", gap: "8px", overflow: "auto" }}>
-                <section style={{ padding: "8px", display: "flex", flex: "1", gap: "8px", justifyContent: "space-between", alignItems: "center" }} >
-                    <h2 id="code">Code</h2>
-                    {themeStr ? <button disabled={clicked} onClick={onClick}>Copy</button> : null}
-                </section>
-                <code>
-                    <pre>
-                        {themeStr}
-                    </pre>
-                </code>
-            </div>
+            <ThemeCode themeStr={themeStr} onThemeSelect={setDockview} activeTheme={theme.name} />
         </div>
 
         <App theme={{ name: dock.name, className: `${dock.className} ${CUSTOM_CLASE}` }} />
